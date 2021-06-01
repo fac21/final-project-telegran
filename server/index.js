97d65f9877769457134e3aa39d406579336264a6
chisha <aamaechi@hotmail.com>
2021-06-01 13:06:49 +0100

    Setup to deploy on Heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const { response } = require("express");
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.get("/slack", (request, response) => {
   response.json({ message: "Hello from server!" });
+});
+
+app.get("*", (request, response) => {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
