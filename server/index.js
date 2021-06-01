a6cc4031bb30dca04fe065834c26ecad77e0779b
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-06-01 15:25:52 +0100

    Add slack message on server start
    Co-authored-by: Chisha <64862888+NewCoderCA@users.noreply.github.com>
    Co-authored-by: Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
    Co-authored-by: Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
    Co-authored-by: chunzg <chunzhang53@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const { response } = require("express");
+const fetch = require('node-fetch');
+const dotenv = require('dotenv')
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+dotenv.config()
 
 app.get("/slack", (request, response) => {
   response.json({ message: "Hello from server!" });
@@ -9,4 +13,28 @@ app.get("/slack", (request, response) => {
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
+  sendSlackbotStartMsg(testMessage);
 });
+
+let testMessage = {
+    "channel": `${process.env.SLACK_CHANNEL_ID}`,
+    "text": "The server has started running!"
+}
+
+function sendSlackbotStartMsg(messageData) {
+  fetch(`${process.env.INCOMING_WEBHOOK_URL}`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "Authorization": 'Bearer ' + process.env.BOT_USER_OAUTH_TOKEN
+    },
+    body: JSON.stringify(messageData),
+    })
+    .then((response) => {
+        if (!response.ok) throw new Error(response.status);
+        return response;
+    })
+    .then((res) => console.log(res.status, res.statusText))
+    .catch((error)=>console.error(error));
+}
+
