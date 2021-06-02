777f13b151ab4b4a9cae39d7d217b98b28be9498
chisha <aamaechi@hotmail.com>
2021-06-02 11:10:11 +0100

    Fixing deployment issues using npm run build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-
-import Navbar from '../../client/src/components/Navbar';
+import Navbar from "../../client/src/components/Navbar";
 // import logo from "./logo.png";
-import "./App.css";
 
-// import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import Connect from "./components/Connect.jsx";
 
-
 function App() {
   return (
     <div className="App">
-
       <header className="App-header">
         {/* <img src={logo} alt="logo" /> */}
         {/* <p>{!data ? "Loading..." : data}</p> */}
@@ -22,7 +17,7 @@ function App() {
           <a href="/connect">Get Started</a>
         </button>
       </header>
-      <Navbar/>
+      <Navbar />
 
       <Router>
         <Switch>
@@ -30,7 +25,6 @@ function App() {
           <Route path="/connect" exact component={() => <Connect />} />
         </Switch>
       </Router>
-
     </div>
   );
 }
