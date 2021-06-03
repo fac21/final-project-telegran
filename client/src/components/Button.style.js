93be68aec887045ee2b71a7e20c956c94c5ec6ac
Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
2021-06-03 15:12:57 +0100

    start styling pages with styled components
    Co-authored-by: chunzg <chunzhang53@gmail.com>

---
+++ client/src/components/Button.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: #73e3c8;
+  margin-top: 2rem;
+  border-radius: 30px;
+  border: none;
+`;
