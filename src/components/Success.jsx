31eecf91ee3285b7ead29d7d157c482d0140693e
chunzg <chunzhang53@gmail.com>
2021-06-08 15:35:16 +0100

    Add confetti feature to Successfully Connected page

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { useWindowSize } from "@react-hook/window-size";
+import Confetti from "react-confetti";
+import { AppContainer } from "./Container.style";
 
 function Success() {
+  const { width, height } = useWindowSize();
+
   return (
-    <div>
+    <AppContainer>
+      <Confetti width={width} height={height} />
       <h1>
         {" "}
         <Link to="/video">Successfully Connected </Link>{" "}
       </h1>
-    </div>
+    </AppContainer>
   );
 }
 
