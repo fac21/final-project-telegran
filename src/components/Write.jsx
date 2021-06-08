d03cfa448413fe04c83cab175eb71d2ce48f163b
Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
2021-06-08 16:51:25 +0100

    change styling
    Co-authored-by: chunzg <chunzhang53@gmail.com>
    Co-authored-by: Chisha <64862888+NewCoderCA@users.noreply.github.com>
    Co-authored-by: Jo Humphrey <31373245+jamdelion@users.noreply.github.com>

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -5,6 +5,7 @@ import voice from "../images/voice.svg";
 import send from "../images/send.svg";
 import profile from "../images/profile.svg";
 import { NavContainer } from "./Nav.style.jsx";
+import { AppContainer } from "./Container.style";
 const fetch = require("node-fetch");
 // const dotenv = require("dotenv");
 // dotenv.config();
@@ -27,31 +28,32 @@ function Write() {
   }
 
   return (
-    <>
+    <div>
       <button>
         <a href="/messages" className="text-link">
           Go back
         </a>
       </button>
-      <div className="messageContainer">
-        <div className="messageTo">
-          To: Jo
-          <img className="avatar" src={profile}></img>
-        </div>
-        <form
-          onSubmit={handleSubmit}
-          onChange={(event) => setMessageContent(event.target.value)}
-        >
-          <textarea
-            id="text"
-            name="text"
-            placeholder="Start typing..."
-          ></textarea>
-          <button type="submit">
-            <Link to="/message-sent">Submit</Link>
-          </button>
-        </form>
-        {/* <form action="/api/slack" method="POST" onChange={(event) => setMessageContent(event.target.value)}>
+      <AppContainer>
+        <div className="messageContainer">
+          <div className="messageTo">
+            To: Jo
+            <img className="avatar" src={profile}></img>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            onChange={(event) => setMessageContent(event.target.value)}
+          >
+            <textarea
+              id="text"
+              name="text"
+              placeholder="Start typing..."
+            ></textarea>
+            <button type="submit">
+              <Link to="/message-sent">Submit</Link>
+            </button>
+          </form>
+          {/* <form action="/api/slack" method="POST" onChange={(event) => setMessageContent(event.target.value)}>
           <textarea placeholder="Start typing..."></textarea>
           <button type="button" onClick={handleSubmit}>Send</button>
           <a href="/success">
@@ -59,26 +61,27 @@ function Write() {
             Send
           </a> */}
 
-        {/* <button
+          {/* <button
             type="submit"
             onClick={() => {
               console.log("works");
               sendSlackbotStartMsg(testMessage);
             }}
           ></button> */}
-        {/* </form> */}
-        <div className="msgButtons">
-          <a href="/speak">
-            {/* we should change this to be state based rather than page */}
-            <img src={voice}></img>
-            <span>Press here to speak your message </span>
-          </a>
+          {/* </form> */}
+          <div className="msgButtons">
+            <a href="/speak">
+              {/* we should change this to be state based rather than page */}
+              <img src={voice}></img>
+              <span>Press here to speak your message </span>
+            </a>
+          </div>
         </div>
-      </div>
-      <NavContainer>
-        <Nav />
-      </NavContainer>
-    </>
+        <NavContainer>
+          <Nav />
+        </NavContainer>
+      </AppContainer>
+    </div>
   );
 }
 
