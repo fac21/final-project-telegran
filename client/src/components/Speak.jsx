47c6212ea3431f6f31e47c5816b418448b8da40a
Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
2021-06-10 14:15:15 +0100

    toggle recording
    Co-authored-by: Jo Humphrey <31373245+jamdelion@users.noreply.github.com>

--- client/src/components/Speak.jsx
+++ client/src/components/Speak.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import Nav from "./Navbar.jsx";
-import microphone from "../images/microphone.svg";
+// import Nav from "./Navbar.jsx";
+// import microphone from "../images/microphone.svg";
 import write from "../images/write.svg";
 import { AppContainer } from "./Container.style";
+import { Button } from "./Button.style.js";
+import profile from "../images/profile.svg";
+import mic from "../images/mic.svg";
+import micoff from "../images/micoff.svg";
+import {
+  MessageContainer,
+  MessageTop,
+  MessageText,
+  MessageButtons,
+} from "./MessageContainer.style.jsx";
 
 import SpeechRecognition, {
   useSpeechRecognition,
@@ -11,45 +21,105 @@ import SpeechRecognition, {
 
 function Speak() {
   const { transcript, resetTranscript } = useSpeechRecognition();
+  const [messageContent, setMessageContent] = React.useState("");
+  const [recording, setRecording] = React.useState("true");
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return <h1>Your browser does not support Speech recognition. </h1>;
   }
 
-  // const [recording, setRecording] = React.useState(false);
   function toggleRecording() {
-    // setRecording(!recording);
+    if (recording) {
+      console.log(recording, "about to stop listening");
+      SpeechRecognition.stopListening();
+    } else {
+      console.log(recording, "about to start listening");
+      SpeechRecognition.startListening({
+        continuous: true,
+      });
+    }
+    setRecording(!recording);
   }
+
+  function handleSubmit(event) {
+    const message = messageContent;
+    event.preventDefault();
+    fetch(`${process.env.REACT_APP_API_URL}/api/write-message`, {
+      method: "POST",
+      body: JSON.stringify({ message }),
+      headers: { "content-type": "application/json" },
+    })
+      .then((response) => {
+        response.json();
+        return <Redirect to="/message-sent" />;
+      })
+      .catch((error) => console.error("Oops!", error));
+  }
+
   return (
     <AppContainer>
-      <p>Go back</p>
-      <p>To: </p>
-      <div className="spokenWords">
-        <button
-          onClick={SpeechRecognition.startListening({
-            continuous: true,
-          })}
-        >
-          Start
-        </button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
+      <Button className="back-button">
+        <a href="/messages" className="text-link">
+          {" "}
+          Go back{" "}
+        </a>
+      </Button>
 
-        <button id="microphone" onClick={toggleRecording}>
-          Recording
-        </button>
-        <img src={microphone} alt="microphone" className="icon"></img>
-        {/* <button id="stopRecording">Stop Recording</button>
+      <MessageContainer>
+        <MessageTop>
+          <span>To:</span>
+          <span></span>
+          Jo<img className="avatar" src={profile}></img>
+        </MessageTop>
+
+        <MessageText>
+          <div className="spokenWords">
+            {/* <button
+              onClick={SpeechRecognition.startListening({
+                continuous: true,
+              })}
+            >
+              Start{" "}
+            </button>
+            <button onClick={SpeechRecognition.stopListening}>Stop</button>
+            <button onClick={resetTranscript}>Reset</button> */}
+
+            <button id="microphone" onClick={toggleRecording}>
+              <img
+                src={recording ? mic : micoff}
+                alt="microphone"
+                className="icon"
+              ></img>
+            </button>
+
+            {/* <button id="stopRecording">Stop Recording</button>
            <img src={microphone} alt="microphone" className="icon"></img>  */}
-      </div>
-      <p>Press here to type your message</p>
-      <p>{transcript}</p>
-      <Link to="/write">
-        <img src={write} alt="write" className="icon"></img>
-        <div>Write</div>
-      </Link>
-      <button id="send">Send</button>
-      <Nav />;
+          </div>
+          <p>Press here to type your message</p>
+
+          <form
+            className="form"
+            onSubmit={handleSubmit}
+            onChange={(event) => setMessageContent(event.target.value)}
+          >
+            <textarea
+              value={transcript}
+              rows="5"
+              cols="20"
+              id="text"
+              name="text"
+              placeholder="Start typing..."
+              className="text-area"
+            ></textarea>
+
+            <Link to="/write">
+              <img src={write} alt="write" className="icon"></img>
+              <div>Write</div>
+            </Link>
+            <button id="send">Send</button>
+          </form>
+        </MessageText>
+      </MessageContainer>
     </AppContainer>
   );
 }
